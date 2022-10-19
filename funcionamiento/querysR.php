1d304a5d4d1dc652bef0373bd35bc7d2965d51a7
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-19 18:03:15 +0100

    Create querysR.php

---
+++ funcionamiento/querysR.php
@@ -0,0 +1,63 @@
+<?php
+require "conexion.php";
+
+function obtenerPros()
+{
+  global $conexion;
+  $productos = array();
+  $query = $conexion->query("SELECT id, nombre FROM productos;");
+  $resultado = $query->fetch(PDO::FETCH_OBJ);
+  while ($resultado != null) {
+    array_push($productos, array(
+      "id" => $resultado->id,
+      "nombre" => $resultado->nombre
+    ));
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+  }
+  return $productos;
+}
+
+function obtenerPro($id)
+{
+  global $conexion;
+  $query = $conexion->query("SELECT * FROM productos WHERE id = '" . $id . "';");
+  $resultado = $query->fetch(PDO::FETCH_OBJ);
+  if ($resultado == null) {
+    header('Location:listado.php?accion=error');
+  }
+  return array(
+    "id" => $resultado->id,
+    "nombre" => $resultado->nombre,
+    "nombre_corto" => $resultado->nombre_corto,
+    "descripcion" => $resultado->descripcion,
+    "pvp" => $resultado->pvp,
+    "familia" => $resultado->familia
+  );
+}
+
+function obtenerFam()
+{
+  global $conexion;
+  $queryF = $conexion->query("SELECT cod, nombre FROM familias;");
+  $resultadoF = $queryF->fetch(PDO::FETCH_OBJ);
+  $familias = array();
+  while ($resultadoF != null) {
+    array_push($familias, array(
+      "cod" => $resultadoF->cod,
+      "nombre" => $resultadoF->nombre
+    ));
+    $resultadoF = $queryF->fetch(PDO::FETCH_OBJ);
+  }
+  return $familias;
+}
+
+function obtenerProFam($pFam)
+{
+  global $conexion;
+  $query = $conexion->query("SELECT * FROM familias WHERE cod = '" . $pFam . "';");
+  $resultado = $query->fetch(PDO::FETCH_OBJ);
+  return array(
+    "cod" => $resultado->cod,
+    "nombre" => $resultado->nombre
+  );
+}
