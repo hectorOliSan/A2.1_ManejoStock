bfc5e7f51ab943bbd68a5124a46c4f6df4619919
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-20 23:19:47 +0100

    Update detalle.php

--- detalle.php
+++ detalle.php
@@ -6,33 +6,51 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Listado de Productos</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
   <?php
   require "funcionamiento/conexion.php";
+  include "funcionamiento/elementos.php";
   include "funcionamiento/querysR.php";
 
-  $accion = $_GET['accion'];
+  $accion = $_GET['accion'] != null ? $_GET['accion'] : header('Location:listado.php');
+  $_GET['accion'] == "Detalle" ? "" : header('Location:listado.php');
   $id = array_key_exists('id', $_GET) ? $_GET['id'] : "";
   $producto = obtenerPro($id);
-  print_r($producto);
+  $familia = obtenerProFam($producto['familia']);
   ?>
 
   <div class="container">
     <br>
-    <!-- ENCABEZADO -->
     <div class="row">
       <div class="col-12">
         <h1 class="font-monospace text-center">Detalle Producto</h1>
       </div>
     </div>
 
+    <div class="card col-8 m-3 mx-auto bg-primary bg-opacity-25">
+      <div class="card-header text-center font-monospace fs-5">
+        <?php echo $producto['nombre'] ?>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title text-center">Código: <?php echo $producto['id'] ?></h5>
+        <p class="card-text">Nombre: <?php echo $producto['nombre'] ?></p>
+        <p class="card-text">Nombre Corto: <?php echo $producto['nombre_corto'] ?></p>
+        <p class="card-text">Código Familia:
+          <?php echo $familia['cod'] ?> - <?php echo $familia['nombre'] ?>
+        </p>
+        <p class="card-text">PVP (€): <?php echo $producto['pvp'] ?></p>
+        <p class="card-text">Descripción: <?php echo $producto['descripcion'] ?></p>
+      </div>
+    </div>
+
     <div class="row">
+      <a class="col-2 mx-auto m-1 btn btn-secondary" href="listado.php">Volver</a>
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
