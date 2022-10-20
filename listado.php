b85fd5e46d33bc5bdcff93e88ca2e212c5bb9ebd
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-20 23:19:49 +0100

    Update listado.php

--- listado.php
+++ listado.php
@@ -6,29 +6,35 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Listado de Productos</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
   <?php
   require "funcionamiento/conexion.php";
-  include "funcionamiento/botones.php";
+  include "funcionamiento/elementos.php";
   include "funcionamiento/querysR.php";
+  include "funcionamiento/querysCUD.php";
+
+  $accion = array_key_exists('accion', $_GET) ? $_GET['accion'] : "";
+  $accion == "Crear" ? crearPro($_GET) : "";
+  $accion == "Actualizar" ? actualizarPro($_GET) : "";
+  $accion == "Borrar" ? borrarPro($_GET) : "";
+
+  if ($accion == "Error_id") {
+    crearAlerta("danger", "<b>Error:</b> El ID no coincide con ningún Producto");
+  }
 
   $productos = obtenerPros();
-
-  print_r($_GET);
   ?>
 
   <div class="container">
     <br>
-    <!-- ENCABEZADO -->
     <div class="row">
       <div class="col-12">
         <h1 class="font-monospace text-center">Gestión de Productos</h1>
       </div>
     </div>
-    <!-- BOTÓN CREAR -->
     <div class="row">
       <div class="col-1">
         <form method="GET" action="formulario.php">
@@ -39,42 +45,41 @@
         </form>
       </div>
     </div>
-    <!<!-- TABLA -->
-      <div class="row">
-        <div class="col-12">
-          <table class="table table-striped align-middle text-center">
-            <thead>
-              <tr>
-                <th>Detalle</th>
-                <th>Código</th>
-                <th>Nombre</th>
-                <th colspan="2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              foreach ($productos as $clave => $valor) {
-                echo "<tr>";
-                echo "<td>";
-                crearBoton("Detalle", "info", "detalle.php", $valor['id']);
-                echo "</td>";
-                echo "<th>" . $valor['id'] . "</th>";
-                echo "<td>" . $valor['nombre'] . "</td>";
-                echo "<td>";
-                crearBoton("Actualizar", "warning", "formulario.php", $valor['id']);
-                echo "</td>";
-                echo "<td>";
-                crearBoton("Borrar", "danger", "listado.php", $valor['id']);
-                echo "</td>";
-                echo "</tr>";
-              }
-              ?>
-            </tbody>
-          </table>
-        </div>
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-striped align-middle text-center">
+          <thead>
+            <tr>
+              <th>Detalle</th>
+              <th>Código</th>
+              <th>Nombre</th>
+              <th colspan="2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            foreach ($productos as $clave => $valor) {
+              echo "<tr>";
+              echo "<td>";
+              crearBoton("Detalle", "info", "detalle.php", $valor['id']);
+              echo "</td>";
+              echo "<th>" . $valor['id'] . "</th>";
+              echo "<td>" . $valor['nombre'] . "</td>";
+              echo "<td>";
+              crearBoton("Actualizar", "warning", "formulario.php", $valor['id']);
+              echo "</td>";
+              echo "<td>";
+              crearBoton("Borrar", "danger", "listado.php", $valor['id']);
+              echo "</td>";
+              echo "</tr>";
+            }
+            ?>
+          </tbody>
+        </table>
       </div>
+    </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
