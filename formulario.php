22e1c62ac1267eecbc8beba811a75329a7acfd8f
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-20 23:19:45 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Listado de Productos</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -14,7 +14,9 @@
   require "funcionamiento/conexion.php";
   include "funcionamiento/querysR.php";
 
-  $accion = $_GET['accion'];
+  $accion = $_GET['accion']!=null ? $_GET['accion'] : header('Location:listado.php');
+  ($_GET['accion'] == "Crear") || ($_GET['accion'] == "Actualizar")
+    ? "" : header('Location:listado.php');
   $id = array_key_exists('id', $_GET) ? $_GET['id'] : "";
 
   $familias = obtenerFam();
@@ -33,7 +35,6 @@
 
   <div class="container">
     <br>
-    <!-- ENCABEZADO -->
     <div class="row">
       <div class="col-12">
         <?php echo "<h1 class='font-monospace text-center'>" . $accion . " Producto</h1>" ?>
@@ -47,23 +48,23 @@
         <div class="row">
           <div class="col-6 mb-3">
             <label for="nombre" class="form-label">Nombre</label>
-            <?php echo "<input type='text' class='form-control' name='nombre' placeholder='Nombre' value='" . $producto['nombre'] . "'>"; ?>
+            <?php echo "<input type='text' class='form-control' name='nombre' placeholder='Nombre' value='" . $producto['nombre'] . "' required>"; ?>
           </div>
 
           <div class="col-6 mb-3">
             <label for="nombre_corto" class="form-label">Nombre Corto</label>
-            <?php echo "<input type='text' class='form-control' name='nombre_corto' placeholder='Nombre Corto' value='" . $producto['nombre_corto'] . "'>"; ?>
+            <?php echo "<input type='text' class='form-control' name='nombre_corto' placeholder='Nombre Corto' value='" . $producto['nombre_corto'] . "' required>"; ?>
           </div>
         </div>
 
         <div class="row">
           <div class="col-6 mb-3">
             <label for="precio" class="form-label">Precio (€)</label>
-            <?php echo "<input type='text' class='form-control' name='precio' placeholder='Precio' value='" . $producto['pvp'] . "'>"; ?>
+            <?php echo "<input type='number' class='form-control' name='pvp' min=0.10 step='0.01' placeholder='Precio' value='" . $producto['pvp'] . "' required>"; ?>
           </div>
           <div class="col-6 mb-3">
             <label for="familia" class="form-label">Familia</label>
-            <select class="form-select" name="familia">
+            <select class="form-select" name="familia" required>
               <?php
               if ($accion == "Actualizar")
                 echo "<option value='" . $fam_producto['cod'] . "' selected hidden>" . $fam_producto['nombre'] . "</option>";
@@ -76,20 +77,23 @@
 
         <div class="col-12 mb-3">
           <label for="descripción" class="form-label">Descripción</label>
-          <?php echo "<textarea class='form-control' name='descripcion' rows='15'>" . $producto['descripcion'] . "</textarea>" ?>
+          <?php echo "<textarea class='form-control' name='descripcion' rows='15' required>" . $producto['descripcion'] . "</textarea>" ?>
         </div>
 
         <?php echo "<input name='accion' type='hidden' value='" . $accion . "'>" ?>
 
         <div class="row">
           <?php echo "<input class='col-1 m-1 btn btn-success' type='submit' value='" . $accion . "'>" ?>
-          <input class="col-1 m-1 btn btn-danger" type="reset" value="Limpiar">
+          <?php
+          if ($accion == "Crear")
+            echo "<input class='col-1 m-1 btn btn-danger' type='reset' value='Limpiar'>";
+          ?>
           <a class="col-1 m-1 btn btn-secondary" href="listado.php">Volver</a>
         </div>
       </form>
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
