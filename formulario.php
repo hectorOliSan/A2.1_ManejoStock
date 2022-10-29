c4b39dc56c91565a88b890d697b95f8be5f53e39
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-29 15:01:30 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -5,13 +5,14 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Listado de Productos</title>
+  <title>Producto</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
   <?php
   require "funcionamiento/conexion.php";
+  include "funcionamiento/elementos.php";
   include "funcionamiento/querysR.php";
 
   $accion = $_GET['accion'] != null ? $_GET['accion'] : header('Location:listado.php');
