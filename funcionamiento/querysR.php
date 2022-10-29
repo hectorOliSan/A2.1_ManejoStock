36dc26610ae6eaa5456625e8dc9f38ed9e42670f
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-29 15:01:44 +0100

    Update querysR.php

--- funcionamiento/querysR.php
+++ funcionamiento/querysR.php
@@ -3,58 +3,140 @@
 function obtenerPros()
 {
   global $conexion;
-  $productos = array();
-  $query = $conexion->query("SELECT id, nombre FROM productos;");
-  $resultado = $query->fetch(PDO::FETCH_OBJ);
-  while ($resultado != null) {
-    array_push($productos, array(
-      "id" => $resultado->id,
-      "nombre" => $resultado->nombre
-    ));
+  try {
+    $productos = array();
+    $query = $conexion->query("SELECT id, nombre FROM productos;");
     $resultado = $query->fetch(PDO::FETCH_OBJ);
+    while ($resultado != null) {
+      array_push($productos, array(
+        "id" => $resultado->id,
+        "nombre" => $resultado->nombre
+      ));
+      $resultado = $query->fetch(PDO::FETCH_OBJ);
+    }
+    return $productos;
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Productos: " . $e);
   }
-  return $productos;
 }
 
 function obtenerPro($id)
 {
   global $conexion;
-  $query = $conexion->query("SELECT * FROM productos WHERE id = '" . $id . "';");
-  $resultado = $query->fetch(PDO::FETCH_OBJ);
-  $resultado == null ? header('Location:listado.php?accion=Error_id') : "";
-  return array(
-    "id" => $resultado->id,
-    "nombre" => $resultado->nombre,
-    "nombre_corto" => $resultado->nombre_corto,
-    "descripcion" => $resultado->descripcion,
-    "pvp" => $resultado->pvp,
-    "familia" => $resultado->familia
-  );
+  try {
+    $query = $conexion->query("SELECT * FROM productos WHERE id = '" . $id . "';");
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+    $resultado == null ? header('Location:listado.php?accion=Error_id') : "";
+    return array(
+      "id" => $resultado->id,
+      "nombre" => $resultado->nombre,
+      "nombre_corto" => $resultado->nombre_corto,
+      "descripcion" => $resultado->descripcion,
+      "pvp" => $resultado->pvp,
+      "familia" => $resultado->familia
+    );
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Producto(" . $id . "): " . $e);
+  }
 }
 
 function obtenerFam()
 {
   global $conexion;
-  $queryF = $conexion->query("SELECT cod, nombre FROM familias;");
-  $resultadoF = $queryF->fetch(PDO::FETCH_OBJ);
-  $familias = array();
-  while ($resultadoF != null) {
-    array_push($familias, array(
-      "cod" => $resultadoF->cod,
-      "nombre" => $resultadoF->nombre
-    ));
+  try {
+    $queryF = $conexion->query("SELECT cod, nombre FROM familias;");
     $resultadoF = $queryF->fetch(PDO::FETCH_OBJ);
+    $familias = array();
+    while ($resultadoF != null) {
+      array_push($familias, array(
+        "cod" => $resultadoF->cod,
+        "nombre" => $resultadoF->nombre
+      ));
+      $resultadoF = $queryF->fetch(PDO::FETCH_OBJ);
+    }
+    return $familias;
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Familias: " . $e);
   }
-  return $familias;
 }
 
 function obtenerProFam($pFam)
 {
   global $conexion;
-  $query = $conexion->query("SELECT * FROM familias WHERE cod = '" . $pFam . "';");
-  $resultado = $query->fetch(PDO::FETCH_OBJ);
-  return array(
-    "cod" => $resultado->cod,
-    "nombre" => $resultado->nombre
-  );
+  try {
+    $query = $conexion->query("SELECT * FROM familias WHERE cod = '" . $pFam . "';");
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+    return array(
+      "cod" => $resultado->cod,
+      "nombre" => $resultado->nombre
+    );
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Familia(" . $pFam . "): " . $e);
+  }
+}
+
+function obtenerStock($id, $tienda, $unidades)
+{
+  global $conexion;
+  try {
+    $query = $conexion->query(
+      "SELECT * FROM stocks
+      WHERE producto='".$id."' AND tienda='".$tienda."';"
+    );
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+    return $resultado;
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Stock del Producto(" . $id . "): " . $e);
+  }
+}
+
+function obtenerProStock($id)
+{
+  global $conexion;
+  try {
+    $stock = array();
+    $query = $conexion->query(
+      "SELECT tiendas.id, tiendas.nombre, stocks.unidades
+      FROM tiendas
+      JOIN stocks ON tiendas.id = stocks.tienda
+      JOIN productos ON productos.id = stocks.producto
+      WHERE productos.id = '" . $id . "' AND stocks.unidades > 0
+      ORDER BY tiendas.nombre;"
+    );
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+    while ($resultado != null) {
+      array_push($stock, array(
+        "tienda_id" => $resultado->id,
+        "tienda" => $resultado->nombre,
+        "unidades" => $resultado->unidades
+      ));
+      $resultado = $query->fetch(PDO::FETCH_OBJ);
+    }
+    return $stock;
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Tiendas y Stock del Producto(" . $id . "): " . $e);
+  }
+}
+
+function obtenerTienda($tienda_id)
+{
+  global $conexion;
+  try {
+    $tiendas = array();
+    $query = $conexion->query("SELECT * FROM tiendas
+      WHERE id != '" . $tienda_id . "';"
+    );
+    $resultado = $query->fetch(PDO::FETCH_OBJ);
+    while ($resultado != null) {
+      array_push($tiendas, array(
+        "id" => $resultado->id,
+        "nombre" => $resultado->nombre,
+        "tlf" => $resultado->tlf
+      ));
+      $resultado = $query->fetch(PDO::FETCH_OBJ);
+    }
+    return $tiendas;
+  } catch (Exception $e) {
+    crearAlerta("danger", "Error al <b>Obtener</b> Tienda(" . $tienda_id . "): " . $e);
+  }
 }
