d9dd7ce515cfd58046beafe16ec024542c4b7ba9
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-10-29 17:36:41 +0100

    Update listado.php

--- listado.php
+++ listado.php
@@ -7,6 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Listado de Productos</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/r-2.3.0/datatables.min.css" />
+
 </head>
 
 <body>
@@ -51,15 +54,18 @@
         </form>
       </div>
     </div>
+    <br>
     <div class="row">
       <div class="col-12">
-        <table class="table table-striped align-middle text-center">
+        <table id="listado" class="display table table-striped align-middle text-center">
           <thead>
             <tr>
-              <th>Detalle</th>
-              <th>Código</th>
-              <th>Nombre</th>
-              <th colspan="3">Acciones</th>
+              <th class="text-center">Detalle</th>
+              <th class="text-center">Código</th>
+              <th class="text-center">Nombre</th>
+              <th></th>
+              <th class="text-center">Acciones</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -69,7 +75,7 @@
               echo "<td>";
               crearBoton("Detalle", "info", "detalle.php", $valor['id']);
               echo "</td>";
-              echo "<th>" . $valor['id'] . "</th>";
+              echo "<td>" . $valor['id'] . "</td>";
               echo "<td>" . $valor['nombre'] . "</td>";
               echo "<td>";
               crearBoton("Actualizar", "warning", "formulario.php", $valor['id']);
@@ -89,6 +95,27 @@
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/r-2.3.0/datatables.min.js" defer></script>
+  <script>
+    $(document).ready(function() {
+      $('#listado').DataTable({
+        language: {
+          search: "Buscar:",
+          lengthMenu: "Mostrando _MENU_ productos",
+          info: "Mostrando de _START_ a _END_ de _TOTAL_ productos",
+          infoEmpty: "Mostrando 0 productos",
+          infoFiltered: "(filtrado de _MAX_ productos totales)",
+          zeroRecords: "No hay productos para mostrar",
+          emptyTable: "No hay datos disponibles",
+          paginate: {
+            previous: "<i class='bi bi-arrow-left-short'></i>",
+            next: "<i class='bi bi-arrow-right-short'></i>",
+          },
+        }
+      });
+    });
+  </script>
 </body>
 
 </html>
