4fc0911c53cb1b46a3059c58063b5aa99d1f93a0
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-11-21 16:16:46 +0000

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -7,14 +7,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Producto</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 
-<body>
-  <?php
-  require "funcionamiento/conexion.php";
-  include "funcionamiento/elementos.php";
-  include "funcionamiento/querysR.php";
+<?php
+require "funcionamiento/conexion.php";
+include "funcionamiento/elementos.php";
+include "funcionamiento/querysR.php";
+session_start();
+include 'funcionamiento/datos_sesion.php';
+?>
 
+<body style="background-color: <?php echo $colorfondo ?>; font-family: <?php echo $tipoletra ?>;">
+  <?php
   $accion = $_GET['accion'] != null ? $_GET['accion'] : header('Location:listado.php');
   ($_GET['accion'] == "Crear") || ($_GET['accion'] == "Actualizar")
     ? "" : header('Location:listado.php');
@@ -33,6 +38,24 @@
   }
 
   ?>
+
+  <div class="d-flex justify-content-between align-items-center p-3">
+    <div class="d-flex align-items-center">
+      <a href="index.php" class="mx-2 btn btn-sm btn-danger"><i class="bi bi-power"></i></a>
+      <p class="m-0">Cerrar Sesión</p>
+    </div>
+    <div class="d-flex align-items-center">
+      <p class="m-0 font-monospace">Usuario: <b><?php echo $nombrecompleto ?></b></p>
+      <form method="POST" action="perfil.php">
+        <input type="hidden" name="php" value="formulario.php">
+        <?php if ($accion == "Actualizar") { ?>
+          <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <?php } ?>
+        <input type="hidden" name="accion" value="<?php echo $accion; ?>">
+        <button type="submit" class="mx-2 btn btn-sm btn-secondary"><i class='bi bi-gear-fill'></i></button>
+      </form>
+    </div>
+  </div>
 
   <div class="container">
     <br>
@@ -86,7 +109,7 @@
 
         <?php echo "<input name='accion' type='hidden' value='" . $accion . "'>" ?>
 
-        <div class="row">
+        <div class="row d-flex justify-content-center">
           <?php echo "<input class='col-1 m-1 btn btn-success' type='submit' value='" . $accion . "'>" ?>
           <?php
           if ($accion == "Crear")
